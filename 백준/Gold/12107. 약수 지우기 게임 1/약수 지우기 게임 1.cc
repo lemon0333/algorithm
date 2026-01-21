7e95d36ed5a85f702c16ae4d115d4d3f0f784d67
Son Hyunbin <147061193+lemonson03@users.noreply.github.com>
2026-01-21 20:45:23 +0900

    [Gold III] Title: 약수 지우기 게임 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12107. 약수 지우기 게임 1/약수 지우기 게임 1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N == 1) {
+        cout << "B" << endl;
+    } else {
+        cout << "A" << endl;
+    }
+
+    return 0;
+}
